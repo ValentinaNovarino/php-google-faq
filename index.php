3156ef76a10de16ee60b3fcf81ffa796b118978a
ValentinaNovarino <valenovarino@gmail.com>
2020-12-16 17:56:29 +0100

    aggiunto dettagli

--- index.php
+++ index.php
@@ -53,7 +53,8 @@ $questions = [
 <html lang="en" dir="ltr">
     <head>
         <meta charset="utf-8">
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&family=Roboto&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="dist/app.css">
         <title>Google-faq</title>
     </head>
